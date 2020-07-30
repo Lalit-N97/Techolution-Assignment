14e026a8a1e1a6a15b68b2a417981d8a9060ecf8
pirate <lalitnegi1997@gmail.com>
2020-07-30 23:59:08 +0530

    created backend data api

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const data = require("./data");
+
+const app = express();
+
+app.get("/api/students", (req, res) => {
+  res.status(200).send(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running at port ", PORT);
+});
